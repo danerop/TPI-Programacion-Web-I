99c599748bf1a0ba3473e9e40b72f6247caef2c4
Hector <danerop@hotmail.com>
2021-07-09 10:43:08 -0300

    arreglo menor

--- js/scripts_calendario.js
+++ js/scripts_calendario.js
@@ -1,40 +1,40 @@
 $(document).ready(function() {
-
-    $('#calendar').fullCalendar({
-      //defaultDate: '2020-01-01', // Muestra el calendario a partir de ese día
-	  defaultDate: moment().format("YYYY-MM-DD"), //Arranca a partir del día actual
-      editable: true,
-      eventLimit: true, // allow "more" link when too many events
-      events: [
-        {
-          title: 'Año Nuevo',
-            url:'https://www.argentina.gob.ar/interior/feriados-nacionales-2020',
-          start: '2020-01-01'
-        },
-		{
-          title: 'Carnaval',
-		  url:'https://www.argentina.gob.ar/interior/feriados-nacionales-2020',
-          start: '2020-02-24',
-          end: '2020-02-26',
-        },
-		{
-          title: 'Feriados con Fines turísticos',
-		  url:'https://www.argentina.gob.ar/interior/feriados-nacionales-2020',
-          start: '2020-03-23',
-          end: '2020-03-25',
-        },
-		{
-          title: 'Día del Veterano y de los Caídos en la Guerra de Malvinas.',
-		  url:'https://www.argentina.gob.ar/interior/feriados-nacionales-2020',
-          start: '2020-03-31'
-        },
-		   {
-          title: 'Viernes Soleado',
-            url:'https://www.unlam.edu.ar',
-          start: '2021-06-25',
-		  end:'2021-06-28',
-        }
-      ]
+    //activa el calendario según las caracteristicas dadas
+    $("#calendar").fullCalendar({
+        defaultDate: moment().format("YYYY-MM-DD"),
+        editable: true,
+        eventLimit: true,
+        eventColor: "green", //Todos los eventos tendran este color por defecto
+        events:[
+            {
+                title: "FERIADO NACIONAL",
+                start: "2021-07-09",
+                color: "red" //pinta de color rojo este evento
+            },
+            {
+                title: "Nuevo curso de GIT",
+                url: "index.html",
+                start: "2021-07-15"
+            },
+            {
+                title: "Inicio de curso de Excel",
+                start: "2021-08-01"
+            },
+            {
+                title: "Actualización del curso de HTML, CSS",
+                url: "curso.html",
+                start: "2021-07-26" 
+            },
+            {
+                title: "Inicio de curso de Piano",
+                url: "curso.html",
+                start: "2021-08-28"
+            },
+            {
+                title: "Inicio de curso de Adobe Photoshop",
+                url: "curso.html",
+                start: "2021-09-6"
+            }
+        ]
     });
-
-  });
+});
