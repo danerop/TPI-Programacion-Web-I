5d68b2e12647924bc784878698b9f7c2480fb9f1
Hector <danerop@hotmail.com>
2021-07-11 15:59:02 -0300

    agregado del limite de 3 formularios

--- js/scripts_formulario.js
+++ js/scripts_formulario.js
@@ -6,9 +6,9 @@ function calcularSueldo(){
     });
     $("#valor_total").html("$ "+num*1000+".-");
 }
-$(document).ready(function(){
 
-   calcularSueldo();
+$(document).ready(function(){
+    calcularSueldo();
     $("#agregar").click(function(){
         var pivot= $(".formulario1").last();
         //clonamos
@@ -17,71 +17,71 @@ $(document).ready(function(){
         $(clon).children().val("");
         //insertamos
         $(clon).clone().insertAfter(pivot);
-        calcularSueldo();      
-    })               
+        calcularSueldo();
+    })
 });
 
 $(document).on("click",".eliminar",function(){
     var padre =$(this).parent(); 
     var pivot= $(".formulario1").first();
-    if(!padre.is(pivot)){
-        padre.remove();
-    }else{
-        padre.children().val("");
+    
+    if( $(".formulario1").toArray().length > 3 ){ //verifica que la cantidad de personas sea mayor a 3
+        if(!padre.is(pivot)){
+            padre.remove();
+        }else{
+            padre.children().val("");
+        }
+        calcularSueldo(); 
     }
-    calcularSueldo(); 
-
 }) 
 
 $(document).ready(function(){
+    //Modal
+    var modal = document.getElementById("myModal");
 
-//Modal
-var modal = document.getElementById("myModal");
+    // Obtén el botón que abre el modal
+    var btn = document.getElementById("insc");
 
-// Obtén el botón que abre el modal
-var btn = document.getElementById("insc");
+    // Obtiene el elemento <span> que cierra el modal
+    var span = document.getElementsByClassName("close")[0];
 
-// Obtiene el elemento <span> que cierra el modal
-var span = document.getElementsByClassName("close")[0];
+    // Cuando el usuario hace clic en el botón, abre el modal 
+    btn.onclick = function() {
+        var contenido = "";
+        var tags = ["Nombre", "Apellido", "Dni"];
+        var i = 0;
+        var cantPer = 1;
 
-// Cuando el usuario hace clic en el botón, abre el modal 
-btn.onclick = function() {
-    var contenido = "";
-    var tags = ["Nombre", "Apellido", "Dni"];
-    var i = 0;
-    var cantPer = 1;
-
-    $('.formulario1').each(function(){
-        contenido = contenido.concat("<ul>");
-        contenido = contenido.concat("<li>PERSONA "+cantPer+"</li>");
+        $('.formulario1').each(function(){
+            contenido = contenido.concat("<ul>");
+            contenido = contenido.concat("<li>PERSONA "+cantPer+"</li>");
         
-       $(this).find("input").each(function(){
-        contenido = contenido.concat("<li>");
-        contenido = contenido.concat(tags[i]+": "+$(this).val()+" ")
-        contenido = contenido.concat("</li>");
-         i++;
-         
-      });
-      contenido = contenido.concat("</ul>");
-      i=0;
-      cantPer +=1;
-        
-     });
-     $("#ingresar_per").html(contenido);
-  modal.style.display = "block";
-  return false;
-}
+            $(this).find("input").each(function(){
+                contenido = contenido.concat("<li>");
+                contenido = contenido.concat(tags[i]+": "+$(this).val()+" ")
+                contenido = contenido.concat("</li>");
+                i++;
+            });
 
-// Cuando el usuario haga clic en <span> (x), cierra el modal
-span.onclick = function() {
-  
-  modal.style.display = "none";
-}
+            contenido = contenido.concat("</ul>");
+            i=0;
+            cantPer +=1;
+        });
 
-// Cuando el usuario haga clic en cualquier lugar fuera del modal, este se cierra
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+        $("#ingresar_per").html(contenido);
+        modal.style.display = "block";
+        return false;
+    }
+
+    // Cuando el usuario haga clic en <span> (x), cierra el modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Cuando el usuario haga clic en cualquier lugar fuera del modal, este se cierra
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
